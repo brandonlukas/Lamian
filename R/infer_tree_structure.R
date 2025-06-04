c891812095e71d4015b7d44b988e0444ed440abd
Brandon Lukas <brandonlukas@gmail.com>
2025-06-04 09:37:12 -0500

    infer_tree_structure can work with custom cluster labels

--- R/infer_tree_structure.R
+++ R/infer_tree_structure.R
@@ -18,12 +18,13 @@
 #' @param  xlab the x-axis labels for the figures.
 #' @param  ylab the y-axis labels for the figures.
 #' @param  max.clunum the maximum number of clusters in the elbew's method.
+#' @param kmeans.seed the seed for kmeans clustering.
+#' @param clusters a named vector of cluster labels for the cells. If NULL (default), then clustering will be performed by kmeans.
 #' @examples
 #' data(hca_bm_pca)
 #' data(hca_bm_saver)
 #' data(hca_bm_cellanno)
-#' res = infer_tree_structure(pca = hca_bm_pca, expression = hca_bm_saver, cellanno = hca_bm_cellanno, origin.marker = c('CD34'), xlab='Principal component 1', ylab = 'Principal component 2')
-
+#' res <- infer_tree_structure(pca = hca_bm_pca, expression = hca_bm_saver, cellanno = hca_bm_cellanno, origin.marker = c("CD34"), xlab = "Principal component 1", ylab = "Principal component 2")
 infer_tree_structure <-
   function(pca,
            cellanno,
@@ -32,10 +33,11 @@ infer_tree_structure <-
            origin.celltype = NA,
            number.cluster = NA,
            plotdir = NA,
-           xlab = 'PC1',
-           ylab = 'PC2',
+           xlab = "PC1",
+           ylab = "PC2",
            max.clunum = 50,
-           kmeans.seed = 12345) {
+           kmeans.seed = 12345,
+           clusters = NULL) {
     alls <- cellanno[, 2]
     names(alls) <- cellanno[, 1]
     ## set.seed(12345)
@@ -43,51 +45,75 @@ infer_tree_structure <-
     x <- seq_len(max.clunum)
     optpoint <- which.min(sapply(2:max.clunum, function(i) {
       x2 <- pmax(0, x - i)
-      sum(lm(sdev[seq_len(max.clunum)] ~ x + x2)$residuals ^ 2)
+      sum(lm(sdev[seq_len(max.clunum)] ~ x + x2)$residuals^2)
     }))
-    pcadim = optpoint + 1
-    pr <- pca[, seq_len(pcadim)]  # 7
-    
-    ## clustering
-    clu <-
-      mykmeans(pr, maxclunum = 50, number.cluster = number.cluster, seed = kmeans.seed)$cluster
-    table(clu)
-    pd = data.frame(x = pr[, 1],
-                    y = pr[, 2],
-                    cluster = as.factor(clu[rownames(pr)]))
-    mypalette = colorRampPalette(brewer.pal(9, 'Set1'))
+    pcadim <- optpoint + 1
+    pr <- pca[, seq_len(pcadim)] # 7
+
+    # ## clustering
+    # clu <-
+    #   mykmeans(pr, maxclunum = 50, number.cluster = number.cluster, seed = kmeans.seed)$cluster
+    # table(clu)
+
+    # --- Modified clustering block ---
+    if (is.null(clusters)) {
+      clu <-
+        mykmeans(pr,
+          maxclunum = 50,
+          number.cluster = number.cluster,
+          seed = kmeans.seed
+        )$cluster
+    } else {
+      if (is.null(names(clusters))) {
+        stop("The 'clusters' vector must be a named vector with cell IDs as names.")
+      }
+      if (!all(rownames(pca) %in% names(clusters))) {
+        stop("All rownames of 'pca' must be present in the names of 'clusters'.")
+      }
+      clu <- clusters[rownames(pca)] # ensure correct ordering
+    }
+    # -------------------------------
+
+    pd <- data.frame(
+      x = pr[, 1],
+      y = pr[, 2],
+      cluster = as.factor(clu[rownames(pr)])
+    )
+    mypalette <- colorRampPalette(brewer.pal(9, "Set1"))
     if (!is.na(plotdir)) {
-      pdf(paste0(plotdir, 'cluster.pdf'),
-          width = 3,
-          height = 2.1)
-        # pdf(paste0(plotdir, 'cluster.pdf'),
-        #   width = 6,
-        #   height = 4.2)
+      pdf(paste0(plotdir, "cluster.pdf"),
+        width = 3,
+        height = 2.1
+      )
+      # pdf(paste0(plotdir, 'cluster.pdf'),
+      #   width = 6,
+      #   height = 4.2)
       print(
         ggplot(data = pd, aes(
-          x = pd[,1], y = pd[,2], color = pd[,3]
+          x = pd[, 1], y = pd[, 2], color = pd[, 3]
         )) +
           geom_scattermore() +
           scale_color_manual(values = mypalette(max(clu))) +
           theme_classic() +
           theme(
-            legend.spacing.y = unit(0.01, 'cm'),
-            legend.spacing.x = unit(0.01, 'cm'),
+            legend.spacing.y = unit(0.01, "cm"),
+            legend.spacing.x = unit(0.01, "cm"),
             legend.key.size = unit(0.1, "cm")
           ) +
-          xlab(xlab) + ylab(ylab)
+          xlab(xlab) +
+          ylab(ylab)
       )
       dev.off()
     }
     ### mclust
     mcl <- exprmclust(t(pr), cluster = clu, reduce = FALSE)
-    
+
     # --------------------
     # construct pseudotime
     # --------------------
     ## find origin
     if (!is.na(origin.celltype)) {
-      pd = data.frame(
+      pd <- data.frame(
         x = pr[, 1],
         y = pr[, 2],
         clu = as.factor(mcl$clusterid),
@@ -99,7 +125,7 @@ infer_tree_structure <-
       pd <- melt(tab)
       pd$clu <-
         factor(as.character(pd$clu), levels = seq(1, max(pd$clu)))
-      tmp <- pd[pd$celltype == origin.celltype,]
+      tmp <- pd[pd$celltype == origin.celltype, ]
       origin.cluster <- as.numeric(tmp[which.max(tmp[, 3]), 1])
     } else {
       pd <-
@@ -111,7 +137,7 @@ infer_tree_structure <-
           stringsAsFactors = FALSE
         )
       tmp <- tapply(pd[, 4], pd[, 3], mean)
-      
+
       origin.cluster <- as.numeric(which.max(tmp))
     }
     ## construct pseudotime
@@ -122,46 +148,54 @@ infer_tree_structure <-
         listbranch = TRUE,
         orderonly = TRUE
       )
-    pt <- unlist(sapply(sapply(ord, length), function(i)
-      seq(1, i)))
+    pt <- unlist(sapply(sapply(ord, length), function(i) {
+      seq(1, i)
+    }))
     names(pt) <- unname(unlist(ord))
-    
+
     # ## plot pseudotime
-    pd = data.frame(pc1 = pca[, 1],
-                    pc2 = pca[, 2],
-                    time = as.numeric(pt[rownames(pca)]))
-    
+    pd <- data.frame(
+      pc1 = pca[, 1],
+      pc2 = pca[, 2],
+      time = as.numeric(pt[rownames(pca)])
+    )
+
     if (!is.na(plotdir)) {
-      pdf(paste0(plotdir, 'pseudotime.pdf'),
-          width = 3.1,
-          height = 2.1)
+      pdf(paste0(plotdir, "pseudotime.pdf"),
+        width = 3.1,
+        height = 2.1
+      )
       print(
         ggplot(data = pd, aes(
-          x = pd[,1], y = pd[,2], color = time
+          x = pd[, 1], y = pd[, 2], color = time
         )) +
           geom_scattermore() +
-          scale_color_gradient(low = 'yellow', high = 'blue') +
-          xlab(xlab) + ylab(ylab) +
+          scale_color_gradient(low = "yellow", high = "blue") +
+          xlab(xlab) +
+          ylab(ylab) +
           theme_classic()
       )
       dev.off()
     }
-    
+
     # ------------------------------------------------------------
     # get candidate branches to test reproducibility, 20200726 >>
     # ------------------------------------------------------------
     newbranch <-
-      findbranch(mst = mcl$MSTtree,
-                 order = ord,
-                 origin = origin.cluster)
+      findbranch(
+        mst = mcl$MSTtree,
+        order = ord,
+        origin = origin.cluster
+      )
     # -----------------------------------------------------
     # Evaluate robustness of tree branches using resampling
     # -----------------------------------------------------
     # null distribution of Jaccard index, overlap coefficient
     js.null <- lapply(seq(1, length(newbranch)), function(i) {
       b.ori <-
-        unlist(sapply(newbranch[[i]], function(c)
-          names(mcl$clusterid[mcl$clusterid == c])))
+        unlist(sapply(newbranch[[i]], function(c) {
+          names(mcl$clusterid[mcl$clusterid == c])
+        }))
       tmp <- sapply(seq(1, 1e3), function(j) {
         ## set.seed(j)
         b.pm <- sample(rownames(pr), length(b.ori))
@@ -169,11 +203,12 @@ infer_tree_structure <-
       })
     })
     js.cut <- sapply(js.null, quantile, 0.99)
-    
+
     oc.null <- lapply(seq(1, length(newbranch)), function(i) {
       b.ori <-
-        unlist(sapply(newbranch[[i]], function(c)
-          names(mcl$clusterid[mcl$clusterid == c])))
+        unlist(sapply(newbranch[[i]], function(c) {
+          names(mcl$clusterid[mcl$clusterid == c])
+        }))
       tmp <- sapply(seq(1, 1e3), function(j) {
         ## set.seed(j)
         b.pm <- sample(rownames(pr), length(b.ori))
--- README.md
+++ README.md
@@ -1,81 +1,73 @@
-# Lamian: a statistical framework for differential pseudotime analysis in multiple single-cell RNA-seq
+# Lamian (Forked by @brandonlukas)
 
-### Wenpin Hou, Zhicheng Ji, Zeyu Chen, E. John Wherry, Stephanie C. Hicks\*, Hongkai Ji\*
-====
+This is a **fork** of the [original Lamian package](https://github.com/Winnie09/Lamian) for pseudotime uncertainty quantification and trajectory inference in single-cell transcriptomic data.
 
-<img src="https://github.com/Winnie09/Lamian/blob/master/vignettes/logo.png"  width = "150">
+## ✨ Key Update in This Fork
 
-## Lamian Installation
+The `infer_tree_structure()` function has been **extended to allow users to supply their own clustering labels**, instead of being restricted to k-means clustering.
 
-Lamian software can be installed via Github.
-Users should have R installed on their computer before installing Lamian. R version needs to be at least 3.5.x or higher. R can be downloaded here: http://www.r-project.org/.
+### 🔧 Why this matters
 
-For Windows users, Rtools is also required to be installed. Rtools can be downloaded here: (https://cloud.r-project.org/bin/windows/Rtools/). For R version 3.5.x, Rtools35.exe is recommended. Use default settings to perform the installation.
+The original function required k-means clustering on PCA-reduced dimensions. However, many single-cell workflows already use robust clustering algorithms like:
+- Seurat’s Louvain/Leiden clustering
+- Scanpy’s community detection methods
+- Hierarchical or graph-based clustering
 
-For mac users, if there is any problem with installation problem, please try download and install clang-8.0.0.pkg from the following URL: https://cloud.r-project.org/bin/macosx/tools/clang-8.0.0.pkg
+This fork gives you full flexibility to use **precomputed cluster assignments**.
 
-For visualization, please download X11 or XQuartz (mac users: https://www.xquartz.org/), and then download the package Cairo using
-```{r}
-install.packages('Cairo')
+---
+
+## 🆕 New Function Signature
+
+```r
+infer_tree_structure(
+  pca,
+  cellanno,
+  expression,
+  origin.marker = NA,
+  origin.celltype = NA,
+  number.cluster = NA,
+  plotdir = NA,
+  xlab = 'PC1',
+  ylab = 'PC2',
+  max.clunum = 50,
+  kmeans.seed = 12345,
+  clusters = NULL  # <-- new argument
+)
 ```
 
-To install the latest version of Lamian package via Github, run the following commands in R:
-```{r }
-## make sure you remove the old version of TSCAN if you've downloaded it before
-if ("TSCAN" %in% rownames(installed.packages())) remove.packages('TSCAN')
-## download dependencies
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-if (!requireNamespace("ComplexHeatmap", quietly = TRUE))
-    BiocManager::install('ComplexHeatmap')
-if (!require("devtools"))
-  install.packages("devtools")
-## downlaod the most updated TSCAN from Github
-devtools::install_github("zji90/TSCAN") 
-## downlaod the most updated Lamian from Github
-devtools::install_github("Winnie09/Lamian")
+### `clusters` (default = `NULL`)
+
+•	If `NULL`, the function falls back to k-means clustering (as in the original Lamian).
+
+•	If a named vector of cluster labels is provided, the function uses these labels directly.
+
+•	The names of the vector must match the rownames of the pca matrix.
+
+## 🧪 Example Usage
+
+### Your PCA-reduced matrix and expression matrix
+```r
+res <- infer_tree_structure(
+  pca = my_pca,
+  cellanno = my_cellanno,
+  expression = my_expression,
+  clusters = my_cluster_labels,  # user-supplied clusters
+  origin.marker = c('CD34'),
+  plotdir = 'plots/',
+  xlab = 'PC1',
+  ylab = 'PC2'
+)
 ```
 
-If there is any problem with the installation process, please make sure you have R version at least 3.5.x and you have installed Rtools (Windows users) or clang (mac users). If the problem still occurs, please contact the author (see below).
+## 🔄 Compatibility
 
+This fork retains full backward compatibility with the original Lamian interface. If clusters is not supplied, k-means clustering will be performed as before.
 
-### Trouble shooting:
+## 📦 Installation
 
-If you encounter difficulties in installing the R package "ComplexHeatmap", then you can download this Github folder and remove the "ComplexHeatmap" in the DESCRIPTION file "Imports:" list to avoid installing it. The function to plot heatmap will not work in this case, but other functions will still be working. And then you run the following lines in terminal (outside the repository "Lamian"):
-```{r}
-R CMD BUILD Lamian
-R CMD INSTALL R CMD INSTALL Lamian_0.99.0.tar.gz
-```
+You can install this forked version using:
 
-## User Manual
-You may use any of the following ways to access user manual:
-(1) check the following page for the user manual: 
-https://winnie09.github.io/Wenpin_Hou/pages/Lamian.html
-
-(2) run the following commands in R, and then open the pop-up window:
-```{r}
-suppressMessages(library(Lamian))
-vignette('Lamian')
-```
-
-![Lamian](vignettes/lamian.jpeg)
-
-## Introductions
-Pseudotime analysis based on single-cell RNA-seq (scRNA-seq) data has been widely used to study dynamic gene regulatory programs along continuous biological processes such as cell differentiation, immune responses, and disease development. Existing pseudotime analysis methods primarily address the issue of reconstructing cellular pseudotemporal trajectories and inferring gene expression changes along the reconstructed trajectory in one biological sample. As scRNA-seq studies are increasingly performed on multiple patient samples, comparing gene expression dynamics across samples has been emerging as a new demand for which a systematic analytical solution is lacking. 
-
-We develop a systematic computational and statistical framework, Lamian, for multi-sample pseudotime analysis. Given scRNA-seq data from multiple biological samples with covariates (e.g., age, sex, sample type, disease status, etc.), this framework allows one to (1) construct cellular pseudotemporal trajectory, evaluate the uncertainty of the trajectory branching structure, (2) evaluate changes in branching structure associated with sample covariates, (3) identify changes in cell abundance and gene expression along the pseudotime, and (4) characterize how sample covariates modifies the pseudotemporal dynamics of cell abundance and gene expression. Importantly, when identifying cell abundance or gene expression changes associated with pseudotime and sample covariates, Lamian accounts for variability across biological samples which other existing pseudotime methods do not consider. As a result, Lamian is able to more appropriately control the false discovery rate (FDR) when analyzing multi-sample data, a property not offered by any other methods. 
-
-![Lamian framework](vignettes/figure1.jpeg)
-
-## Citation 
-
-A statistical framework for differential pseudotime analysis with multiple single-cell RNA-seq samples. 
-Wenpin Hou, Zhicheng Ji, Zeyu Chen, E John Wherry, Stephanie C Hicks\*, Hongkai Ji\*. 
-bioRxiv 2021.07.10.451910; doi: https://doi.org/10.1101/2021.07.10.451910. 
-
-This manuscript is now under revision in a peer-review journal.
-
-## Contact
-
-Should you encounter any bugs or have any suggestions, please feel free to contact Wenpin Hou <wh2526@cumc.columbia.edu>, or open an issue on the Github page https://github.com/Winnie09/Lamian/issues.
+### If using devtools
+`devtools::install_github("brandonlukas/Lamian")`
 
